5048ef62c502d4ba0fdb0f9dab42d35f7e210058
Thongttdev <thongtt0409@gmail.com>
2023-02-28 00:00:30 +0700

    fix production

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8000;
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://63fcdcec60e9610098d7a611--stupendous-florentine-e8a8a8.netlify.app/',
     credentials: true,
   })
 );

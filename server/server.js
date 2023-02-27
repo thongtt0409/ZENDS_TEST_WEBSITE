c7150eff2a4a1c16e0092c86ea514ffb6dd81d87
Thongttdev <thongtt0409@gmail.com>
2023-02-27 20:54:37 +0700

    fix cors origin localhost

--- server/server.js
+++ server/server.js
@@ -9,12 +9,7 @@ const cors = require('cors');
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 mongoose
   .connect(process.env.MONGO_URL)

50b51c8252bb2d79c3876f241c1a18e4693d7045
thongtt0409 <84649559+thongtt0409@users.noreply.github.com>
2023-02-28 11:24:15 +0700

    update cors

--- server/server.js
+++ server/server.js
@@ -9,15 +9,7 @@ const cors = require('cors');
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
-app.use(
-  cors({
-    origin: [
-      'https://63fcdcec60e9610098d7a611--stupendous-florentine-e8a8a8.netlify.app',
-      'http://localhost:3000',
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 mongoose
